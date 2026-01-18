c0c00f7a92970bed16a2e99645e76601a393b218
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2026-01-18 20:04:42 +0530

    updates done

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -15,23 +15,50 @@ writeFileSync ("/db.json",JSON.stringify(data))
 
 orderRouter.post("/", (req,res)=>{
     
-    const data = readFile();
+    const data = readData();
 
     newOrder={
 
         id: Date.now().toString(),
-        productId:
-        quantity:
+        productId: req.body.productId
+        quantity: req.body.quantity
         totalAmount:product.price * quantity,
         status:
-        createdAt: Date();
+        createdAt: Date.now();
     }
     data.orders.push(newOrder)
-    writeFile (data);
-    res.status(201).json("new order created")
+    writeData (data);
+    res.status(201).json({message:"new order created"})
+});
+
+
+orderRouter.get("/", (req,res)=>{
+const data = readData()
+res.status(200).json({message:"all orders"})
+
+});
+
+orderRouter.delete("/:orderId",(res,req)=>{
+const data= readData()
+const filteredOrders= data.orders.filter(order=> order.id !== req.params.orderId)
+if(filteredOrders.length === data.orders.length || order.createdAt === currentDate)
+    return res.status(400).json({message:"order not found"})
 })
 
+data.orders=filteredOrders;
+writeData(data);
+res.status(200).json({message:"cancelled order successfully"})
 
-router
+orderRouter.patch("/change-status/:orderId", (res,req)=>{
+const data= writeData();
 
+const userIndex= data.orders.findIndex(order=> order.id=== req.params.orderId)
+if (userIndex === -1){
+    return res.status(404).send({message:"order not found"});
+}
+const existingOrder = orders[orderIndex];
+order[OrderIndex] ={...existingOrder, ...req.body};
 
+res.status(200).json(orders[orderIndex]);
+
+});
