49003e685f22d7c465a3486e9f0c5bb718c0a4aa
ramalakshmi0304 <mlaksmipradeep@gmail.com>
2026-01-18 19:31:27 +0530

    updates done

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -13,9 +13,25 @@ const writeFile =(data)=>{
 writeFileSync ("/db.json",JSON.stringify(data))
 };
 
-orderRouter("/", (req,res)=>{
+orderRouter.post("/", (req,res)=>{
+    
+    const data = readFile();
 
+    newOrder={
 
+        id: Date.now().toString(),
+        productId:
+        quantity:
+        totalAmount:product.price * quantity,
+        status:
+        createdAt: Date();
+    }
+    data.orders.push(newOrder)
+    writeFile (data);
+    res.status(201).json("new order created")
 })
+
+
+router
 
 
